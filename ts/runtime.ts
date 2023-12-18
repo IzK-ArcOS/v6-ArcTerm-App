d9ba2ffabfed2a363fab9255b4840cec32014a15
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-18 17:51:52 +0100

    Remove mutator test

--- ts/runtime.ts
+++ ts/runtime.ts
@@ -4,16 +4,5 @@ import { AppRuntime } from "$ts/apps";
 export class Runtime extends AppRuntime {
   constructor(app: App, mutator: AppMutator) {
     super(app, mutator);
-
-    mutator.update((m) => {
-      m.size.w = 0;
-      m.size.h = 0;
-      m.minSize.w = 0;
-      m.minSize.h = 0;
-      m.maxSize.w = 0;
-      m.maxSize.h = 0;
-
-      return m;
-    })
   }
 }
